8657daf6ceb16f11e0bfdbf8b546444fc262da05
Анна <mambarinka@mail.ru>
2020-06-18 16:29:00 +0400

    минимифицирует html и js

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
+var htmlmin = require("gulp-htmlmin");
+var uglify = require("gulp-uglify");
 var server = require("browser-sync").create();
 
 var ghPages = require("gh-pages");
@@ -72,7 +74,17 @@ gulp.task("html", function () {
     .pipe(posthtml([
       include()
     ]))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
     .pipe(gulp.dest("build"));
+});
+
+gulp.task("js", function () {
+  return gulp.src("source/js/*.js")
+    .pipe(uglify())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("build/js"))
 });
 
 gulp.task("copy", function () {
